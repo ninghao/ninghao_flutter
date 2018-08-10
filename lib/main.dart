0e064d501773037c2c7a79dfabfc36a804f46cea
wanghao8080 <117663444@qq.com>
2018-08-10 15:12:04 +0800

    MaterialApp：使用界面组件与定制界面主题

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,34 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: Text(
-        'hello',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.yellow,
-        )
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('NINGHAO'),
+          elevation: 0.0,
+        ),
+        body: Hello()
       ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      )
     );
   }
+}
+
+class Hello extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      return Center(
+        child: Text(
+          'hello',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          )
+        ),
+      );
+    }
 }
