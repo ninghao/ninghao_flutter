b335a5563344de7e062a3dc4988f08fb8a2ad6dc
wanghao8080 <117663444@qq.com>
2018-08-12 12:54:53 +0800

    TabBar：用标签形式展示内容

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,41 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigration',
-          onPressed: () => debugPrint('Navigration button is pressed.'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigration',
+            onPressed: () => debugPrint('Navigration button is pressed.'),
+          ),
+          title: Text('NINGHAO'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Search button is pressed.'),
+            )
+          ],
+          elevation: 0.0,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
         ),
-        title: Text('NINGHAO'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: () => debugPrint('Search button is pressed.'),
-          )
-        ],
-        elevation: 0.0,
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
+        ),
       ),
-      body: null,
     );
   }
 }
