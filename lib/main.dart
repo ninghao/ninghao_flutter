45eb28f644e7bb481f378974825d4788e5e3ba25
wanghao8080 <117663444@qq.com>
2018-08-12 12:59:01 +0800

    TabBar：自定义标签栏的样式

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,10 @@ class Home extends StatelessWidget {
           ],
           elevation: 0.0,
           bottom: TabBar(
+            unselectedLabelColor: Colors.black38,
+            indicatorColor: Colors.black54,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
             tabs: <Widget>[
               Tab(icon: Icon(Icons.local_florist)),
               Tab(icon: Icon(Icons.change_history)),
