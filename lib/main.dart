1f086b698af1bba71d7489d01a8f05f406e97c08
wanghao8080 <117663444@qq.com>
2018-08-13 20:08:00 +0800

    用户帐户头部的背景图像

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,17 @@ class Home extends StatelessWidget {
                 currentAccountPicture: CircleAvatar(
                   backgroundImage: NetworkImage('https://resources.ninghao.org/images/wanghao.jpg'),
                 ),
+                decoration: BoxDecoration(
+                  color: Colors.yellow[400],
+                  image: DecorationImage(
+                    image: NetworkImage('https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.yellow[400].withOpacity(0.6),
+                      BlendMode.hardLight
+                    ),
+                  ),
+                ),
               ),
               ListTile(
                 title: Text('Messages', textAlign: TextAlign.right,),
