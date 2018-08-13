5158e3feb1053c0e7763e8d6969def85b1ec47e9
wanghao8080 <117663444@qq.com>
2018-08-13 21:16:35 +0800

    BottomNavigrationBar：底部导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,29 @@ class Home extends StatelessWidget {
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
-        drawer: DrawerDemo()
+        drawer: DrawerDemo(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title: Text('Explore'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title: Text('History'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('List'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('My'),
+            ),
+          ],
+        ),
       ),
     );
   }
