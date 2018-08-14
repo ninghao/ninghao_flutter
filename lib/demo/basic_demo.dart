437a479aaaa1b1cd264179375c53e90d67f98167
wanghao8080 <117663444@qq.com>
2018-08-14 11:58:05 +0800

    Text：文字与文字样式

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class BasicDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 16.0,
+  );
+
+  final String _author = '李白';
+  final String _title = '将进酒';
+  
   @override
   Widget build(BuildContext context) {
     return Text(
-      '君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。',
+      '《 $_title 》—— $_author。君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。',
       textAlign: TextAlign.left,
+      style: _textStyle,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
