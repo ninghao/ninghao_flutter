9f48dc2d215ba1a5399fff7882d7304038f11e28
wanghao8080 <117663444@qq.com>
2018-08-14 11:19:10 +0800

    Flutter 基础：准备项目

---
+++ lib/demo/basic_demo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('hello');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './demo/drawer_demo.dart';
 import './demo/bottom_navigation_bar_demo.dart';
 import './demo/listview_demo.dart';
+import './demo/basic_demo.dart';
 
 void main() => runApp(App());
 
@@ -52,7 +53,8 @@ class Home extends StatelessWidget {
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            // Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            BasicDemo(),
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
@@ -62,4 +64,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
