2e10042ab2075a9f91277ab0b118d1aa4d7c8dee
wanghao8080 <117663444@qq.com>
2018-08-14 11:44:46 +0800

    查看小部件的定义

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('hello');
+    return Text(
+      '君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。',
+      textAlign: TextAlign.left,
+    );
   }
 }
