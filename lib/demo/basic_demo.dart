100a391de60f027ff848cb5336d3b663a156f2ab
wanghao8080 <117663444@qq.com>
2018-08-15 08:54:48 +0800

    Container：容器

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -3,7 +3,21 @@ import 'package:flutter/material.dart';
 class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return RichTextDemo();
+    return Container(
+      color: Colors.grey[100],
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.pool, size: 32.0, color: Colors.white),
+            color: Color.fromRGBO(3, 54, 255, 1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+          ),
+        ],
+      ),
+    );
   }
 }
 
