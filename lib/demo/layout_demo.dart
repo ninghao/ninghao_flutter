1336a0720164e9b60c2d2c8dbcf0064310152b97
wanghao8080 <117663444@qq.com>
2018-08-17 20:22:41 +0800

    Row（横排）与 Column（竖排）

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        IconBadge(Icons.pool)
-      ],
+    return Container(
+      child: Column(
+        children: <Widget>[
+          IconBadge(Icons.pool),
+          IconBadge(Icons.beach_access),
+          IconBadge(Icons.airplanemode_active),
+        ],
+      ),
     );
   }
 }
