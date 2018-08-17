bbe84f84ce6c4a252e0e5650fe6ed63bead2f01d
wanghao8080 <117663444@qq.com>
2018-08-17 20:19:24 +0800

    创建可配置的图标徽章（IconBadge）小部件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        IconBadge(Icons.pool)
+      ],
+    );
   }
+}
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon, {
+    this.size = 32.0
+  });
+
+  @override
+    Widget build(BuildContext context) {
+      return Container(
+        child: Icon(icon, size: size, color: Colors.white),
+        width: size + 60,
+        height: size + 60,
+        color: Color.fromRGBO(3, 54, 255, 1.0),
+      );
+    }
 }
