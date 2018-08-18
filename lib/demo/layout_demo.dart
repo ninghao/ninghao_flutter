040482f9f5462dbc65b7bb3c5c6d0bc57e8d1af3
wanghao8080 <117663444@qq.com>
2018-08-18 09:48:22 +0800

    SizedBox：固定尺寸的盒子

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -5,12 +5,33 @@ class LayoutDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          IconBadge(Icons.pool),
-          IconBadge(Icons.beach_access, size: 64.0),
-          IconBadge(Icons.airplanemode_active),
+          SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
+            ),
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          SizedBox(
+            width: 100.0,
+            height: 100.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
+            ),
+          ),
         ],
       ),
     );
