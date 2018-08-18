b372c9e8e9161bea1b7fdc66c8155edd0c2631c5
wanghao8080 <117663444@qq.com>
2018-08-18 11:53:32 +0800

    创建 AspectRatioDemo 小部件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,13 +7,20 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          AspectRatio(
-            aspectRatio: 16.0/9.0,
-            child: Container(
-              color: Color.fromRGBO(3, 54, 255, 1.0),
-            ),
-          ),
+          AspectRatioDemo()
         ],
+      ),
+    );
+  }
+}
+
+class AspectRatioDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 16.0 / 9.0,
+      child: Container(
+        color: Color.fromRGBO(3, 54, 255, 1.0),
       ),
     );
   }
