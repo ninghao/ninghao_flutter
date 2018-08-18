0ef69a55dcd496bf1fdb2823d5225964a76e5c54
wanghao8080 <117663444@qq.com>
2018-08-18 11:20:38 +0800

    Stack：一摞小部件

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,31 +7,75 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              alignment: Alignment(0.0, -0.9),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  alignment: Alignment(0.0, -0.9),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0),
-            ),
-          ),
-          SizedBox(
-            height: 32.0,
-          ),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+              SizedBox(
+                height: 32.0,
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
-            ),
+              SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(colors: [
+                      Color.fromRGBO(7, 102, 255, 1.0),
+                      Color.fromRGBO(3, 54, 255, 1.0),
+                    ]),
+                  ),
+                  child:
+                      Icon(Icons.brightness_2, color: Colors.white, size: 32.0),
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+              Positioned(
+                right: 40.0,
+                top: 60.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 18.0),
+              ),
+              Positioned(
+                right: 20.0,
+                top: 120.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 20.0),
+              ),
+              Positioned(
+                right: 70.0,
+                top: 180.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+              Positioned(
+                right: 30.0,
+                top: 230.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 18.0),
+              ),
+              Positioned(
+                right: 90.0,
+                bottom: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+              Positioned(
+                right: 4.0,
+                bottom: -4.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 16.0),
+              ),
+            ],
           ),
         ],
       ),
@@ -43,17 +87,15 @@ class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBadge(this.icon, {
-    this.size = 32.0
-  });
+  IconBadge(this.icon, {this.size = 32.0});
 
   @override
-    Widget build(BuildContext context) {
-      return Container(
-        child: Icon(icon, size: size, color: Colors.white),
-        width: size + 60,
-        height: size + 60,
-        color: Color.fromRGBO(3, 54, 255, 1.0),
-      );
-    }
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(icon, size: size, color: Colors.white),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
 }
