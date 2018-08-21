ac08466b2e7aa11860dda3ced1e6173df6c7937d
wanghao8080 <117663444@qq.com>
2018-08-21 20:57:32 +0800

    PageView：页面视图

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,6 +3,33 @@ import 'package:flutter/material.dart';
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageView(
+      children: <Widget>[
+        Container(
+          color: Colors.brown[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'ONE',
+            style: TextStyle(fontSize: 32.0, color: Colors.white)
+          ),
+        ),
+        Container(
+          color: Colors.grey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'TWO',
+            style: TextStyle(fontSize: 32.0, color: Colors.white)
+          ),
+        ),
+        Container(
+          color: Colors.blueGrey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'THREE',
+            style: TextStyle(fontSize: 32.0, color: Colors.white)
+          ),
+        ),
+      ],
+    );
   }
 }
