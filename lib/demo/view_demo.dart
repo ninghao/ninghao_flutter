6e9fe34b4d90f9b90acaa8bbbba18e32905dbd19
wanghao8080 <117663444@qq.com>
2018-08-21 23:23:02 +0800

    PageView.builder 按需生成页面

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold)
+              ),
+              Text(
+                posts[index].author
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return PageViewDemo();
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
   }
 }
 
