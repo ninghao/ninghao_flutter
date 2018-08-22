0e148349dab9644b7a20e7aa8522b37037d944b4
wanghao8080 <117663444@qq.com>
2018-08-22 13:28:07 +0800

    GridView.extent

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -4,7 +4,31 @@ import '../model/post.dart';
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridViewCountDemo();
+    return GridViewExtentDemo();
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text('Item $index',
+            style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 150.0,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      // scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
   }
 }
 
