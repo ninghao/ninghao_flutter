2076afa6f2af3a20014df1adf97fb7a46bc8439d
wanghao8080 <117663444@qq.com>
2018-08-22 12:52:40 +0800

    GridView：网格视图

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child:
+            Text('Item $index', style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageViewBuilderDemo();
+    return GridView.count(
+      crossAxisCount: 3,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
+    );
   }
 }
 
