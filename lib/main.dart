f0107ba4b8aa28c267b423339198255a026cc066
wanghao8080 <117663444@qq.com>
2018-08-23 09:38:39 +0800

    创建 SliverDemo 小部件

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './demo/listview_demo.dart';
 import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
+import './demo/sliver_demo.dart';
 
 void main() => runApp(App());
 
@@ -13,7 +14,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: SliverDemo(),
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
