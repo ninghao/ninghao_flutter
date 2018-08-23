11cc5798943b03404c4babddb53d316a4a52edeb
wanghao8080 <117663444@qq.com>
2018-08-23 14:18:27 +0800

    SliverList：列表视图

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -10,10 +10,65 @@ class SliverDemo extends StatelessWidget {
           SliverSafeArea(
             sliver: SliverPadding(
               padding: EdgeInsets.all(8.0), 
-              sliver: SliverGridDemo()
+              sliver: SliverListDemo()
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 32.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(12.0),
+              elevation: 14.0,
+              shadowColor: Colors.grey.withOpacity(0.5),
+              child: Stack(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 16/9,
+                    child: Image.network(
+                      posts[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 32.0,
+                    left: 32.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          posts[index].title,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                          ),
+                        ),
+                        Text(
+                          posts[index].author,
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.white
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        childCount: posts.length,
       ),
     );
   }
