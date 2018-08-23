3e7042afa4923277bd1443a9b51e2338dfe93eb7
wanghao8080 <117663444@qq.com>
2018-08-23 12:35:50 +0800

    SliverGrid：网格视图

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class SliverDemo extends StatelessWidget {
   @override
@@ -6,7 +7,25 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              childAspectRatio: 1.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  child: Image.network(
+                    posts[index].imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+              childCount: posts.length,
+            ),
+          ),
         ],
       ),
     );
