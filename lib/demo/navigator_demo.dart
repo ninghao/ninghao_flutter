39504b0e561001bafa91a5a67de50d433477d6ec
wanghao8080 <117663444@qq.com>
2018-08-28 13:05:22 +0800

    打开新页面并返回（push 与 pop）：准备

---
+++ lib/demo/navigator_demo.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class NavigatorDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text('Home'),
+              onPressed: null,
+            ),
+            FlatButton(
+              child: Text('About'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  final String title;
+
+  Page({
+    this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './demo/basic_demo.dart';
 import './demo/layout_demo.dart';
 import './demo/view_demo.dart';
 import './demo/sliver_demo.dart';
+import './demo/navigator_demo.dart';
 
 void main() => runApp(App());
 
@@ -14,7 +15,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: NavigatorDemo(),
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
