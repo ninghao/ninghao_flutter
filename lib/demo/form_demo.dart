298f94fe2483267f5f941d911d4dc721f692aa06
wanghao8080 <117663444@qq.com>
2018-08-30 15:10:50 +0800

    TextField：文本字段

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -8,9 +8,29 @@ class FormDemo extends StatelessWidget {
         data: Theme.of(context).copyWith(
           primaryColor: Colors.black,
         ),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFieldDemo(),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget{
+  TextFieldDemoState createState()=>  TextFieldDemoState();
+}
+
+
+class TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context){
+    return TextField();
   }
 }
 
