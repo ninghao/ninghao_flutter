4af65b17f994071a7f17be32bc323260e1ff8d14
wanghao8080 <117663444@qq.com>
2018-09-01 20:57:56 +0800

    FloatingActionButton：漂浮动作按钮

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -20,11 +20,28 @@ class MaterialComponents extends StatelessWidget {
 class FloatingActionButtonDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Widget _floatingActionButton = FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.add),
+      elevation: 0.0,
+      backgroundColor: Colors.black87,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0)
+      ),
+    );
+
+    final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+      onPressed: () {},
+      icon: Icon(Icons.add),
+      label: Text('Add'),
+    );
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('FloatingActionButtonDemo'),
         elevation: 0.0,
       ),
+      floatingActionButton: _floatingActionButtonExtended,
     );
   }
 }
