92e7ae20ccf4cd0b306311c4f1bf9c2d4981ec3c
wanghao8080 <117663444@qq.com>
2018-09-02 16:41:40 +0800

    按钮容器 Container：有固定宽度的按钮

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -114,6 +114,25 @@ class ButtonDemo extends StatelessWidget {
       ],
     );
 
+    final Widget FixedWidthButton = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 130.0,
+          child: OutlineButton(
+            child: Text('Button'),
+            onPressed: () {},
+            splashColor: Colors.grey[100],
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+            textColor: Colors.black,
+            highlightedBorderColor: Colors.grey,
+          ),
+        ),
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('ButtonDemo'),
@@ -127,17 +146,8 @@ class ButtonDemo extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Theme(
-                  data: Theme.of(context).copyWith(
-                    buttonColor: Theme.of(context).accentColor,
-                    buttonTheme: ButtonThemeData(
-                      textTheme: ButtonTextTheme.primary,
-                      // shape: BeveledRectangleBorder(
-                      //   borderRadius: BorderRadius.circular(5.0),
-                      // ),
-                      shape: StadiumBorder(),
-                    ),
-                  ),
+                Container(
+                  width: 130.0,
                   child: OutlineButton(
                     child: Text('Button'),
                     onPressed: () {},
@@ -145,19 +155,9 @@ class ButtonDemo extends StatelessWidget {
                     borderSide: BorderSide(
                       color: Colors.black,
                     ),
-                    // color: Theme.of(context).accentColor,
                     textColor: Colors.black,
                     highlightedBorderColor: Colors.grey,
-                    // textTheme: ButtonTextTheme.primary,
                   ),
-                ),
-                SizedBox(width: 16.0,),
-                OutlineButton.icon(
-                  icon: Icon(Icons.add),
-                  label: Text('Button'),
-                  onPressed: () {},
-                  splashColor: Colors.grey,
-                  textColor: Theme.of(context).accentColor,
                 ),
               ],
             ),
