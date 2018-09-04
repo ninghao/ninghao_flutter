76a00762af45a68b3b7c3b7bed5b701418148804
wanghao8080 <117663444@qq.com>
2018-09-04 20:19:28 +0800

    Slider：滑动选择器

--- lib/demo/slider_demo.dart
+++ lib/demo/slider_demo.dart
@@ -6,6 +6,8 @@ class SliderDemo extends StatefulWidget {
 }
 
 class _SliderDemoState extends State<SliderDemo> {
+  double _sliderItemA = 0.0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +23,24 @@ class _SliderDemoState extends State<SliderDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                
+                Slider(
+                  value: _sliderItemA,
+                  onChanged: (value) {
+                    setState(() {
+                      _sliderItemA = value;
+                    });
+                  },
+                  activeColor: Theme.of(context).accentColor,
+                  inactiveColor: Theme.of(context).accentColor.withOpacity(0.3),
+                  min: 0.0,
+                  max: 10.0,
+                  divisions: 10,
+                  label: '${_sliderItemA.toInt()}',
+                ),
               ],
             ),
+            SizedBox(height: 16.0,),
+            Text('SliderValue: $_sliderItemA'),
           ],
         ),
       )
