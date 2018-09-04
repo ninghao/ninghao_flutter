47ac39184d718316bbda98aa782ade268867d3d2
wanghao8080 <117663444@qq.com>
2018-09-04 13:37:45 +0800

    准备 RadioDemo

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -4,6 +4,7 @@ import './floating_action_button_demo.dart';
 import './popup_menu_button_demo.dart';
 import './form_demo.dart';
 import './checkbox_demo.dart';
+import './radio_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -15,6 +16,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'Radio', page: RadioDemo()),
           ListItem(title: 'Checkbox', page: CheckboxDemo()),
           ListItem(title: 'Form', page: FormDemo()),
           ListItem(title: 'PopupMenuButton', page: PopupMenuButtonDemo()),
---
+++ lib/demo/radio_demo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class RadioDemo extends StatefulWidget {
+  @override
+  _RadioDemoState createState() => _RadioDemoState();
+}
+
+class _RadioDemoState extends State<RadioDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RadioDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
