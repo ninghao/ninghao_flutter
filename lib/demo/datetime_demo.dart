6a8f4dc18dfb14f0b507cc2994675cb4b6f320cf
wanghao8080 <117663444@qq.com>
2018-09-05 09:47:38 +0800

    创建 DateTime

---
+++ lib/demo/datetime_demo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DateTimeDemo extends StatefulWidget {
+  @override
+  _DateTimeDemoState createState() => _DateTimeDemoState();
+}
+
+class _DateTimeDemoState extends State<DateTimeDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DateTimeDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -7,6 +7,7 @@ import './checkbox_demo.dart';
 import './radio_demo.dart';
 import './switch_demo.dart';
 import './slider_demo.dart';
+import './datetime_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -18,6 +19,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'Date & Time', page: DateTimeDemo()),
           ListItem(title: 'Slider', page: SliderDemo()),
           ListItem(title: 'Switch', page: SwitchDemo()),
           ListItem(title: 'Radio', page: RadioDemo()),
