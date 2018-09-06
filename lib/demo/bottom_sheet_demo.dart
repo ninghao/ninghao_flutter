7d9262fdd58422a6bda622b9450dd8ce42459c7c
wanghao8080 <117663444@qq.com>
2018-09-06 20:00:15 +0800

    BottomSheet：底部滑动窗口

--- lib/demo/bottom_sheet_demo.dart
+++ lib/demo/bottom_sheet_demo.dart
@@ -6,9 +6,36 @@ class BottomSheetDemo extends StatefulWidget {
 }
 
 class _BottomSheetDemoState extends State<BottomSheetDemo> {
+  final _bottomSheetScaffoldKey = GlobalKey<ScaffoldState>();
+  
+  _openBottomSheet() {
+    _bottomSheetScaffoldKey
+      .currentState
+      .showBottomSheet((BuildContext context) {
+        return BottomAppBar(
+          child: Container(
+            height: 90.0,
+            width: double.infinity,
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.pause_circle_outline),
+                SizedBox(width: 16.0,),
+                Text('01:30 / 03:30'),
+                Expanded(
+                  child: Text('Fix you - Coldplay', textAlign: TextAlign.right,),
+                ),
+              ],
+            ),
+          ),
+        );
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _bottomSheetScaffoldKey,
       appBar: AppBar(
         title: Text('BottomSheetDemo'),
         elevation: 0.0,
@@ -21,7 +48,10 @@ class _BottomSheetDemoState extends State<BottomSheetDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-
+                FlatButton(
+                  child: Text('Open BottomSheet'),
+                  onPressed: _openBottomSheet,
+                ),
               ]
             ),
           ],
