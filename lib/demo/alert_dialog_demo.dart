f9662a12df73b43096f22a5738da246d9a5f6dd3
wanghao8080 <117663444@qq.com>
2018-09-06 13:11:04 +0800

    AlertDialog：提示对话框

--- lib/demo/alert_dialog_demo.dart
+++ lib/demo/alert_dialog_demo.dart
@@ -6,6 +6,33 @@ class AlertDialogDemo extends StatefulWidget {
 }
 
 class _AlertDialogDemoState extends State<AlertDialogDemo> {
+  _openAlertDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog'),
+          content: Text('Are you sure about this?'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +48,10 @@ class _AlertDialogDemoState extends State<AlertDialogDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                RaisedButton(
+                  child: Text('Open AlertDialog'),
+                  onPressed: _openAlertDialog,
+                ),
               ],
             ),
           ],
