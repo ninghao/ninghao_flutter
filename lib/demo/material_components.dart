14134dd154a97979227edb7af5314b870a50db9d
wanghao8080 <117663444@qq.com>
2018-09-06 22:21:00 +0800

    创建 SnackBarDemo

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -11,6 +11,7 @@ import './datetime_demo.dart';
 import './simple_dialog_demo.dart';
 import './alert_dialog_demo.dart';
 import './bottom_sheet_demo.dart';
+import './snack_bar_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -22,6 +23,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'SnackBar', page: SnackBarDemo()),
           ListItem(title: 'BottomSheet', page: BottomSheetDemo()),
           ListItem(title: 'AlertDialog', page: AlertDialogDemo()),
           ListItem(title: 'SimpleDialog', page: SimpleDialogDemo()),
---
+++ lib/demo/snack_bar_demo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SnackBarDemo extends StatefulWidget {
+  @override
+  _SnackBarDemoState createState() => _SnackBarDemoState();
+}
+
+class _SnackBarDemoState extends State<SnackBarDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SnackBarDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+              ]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
