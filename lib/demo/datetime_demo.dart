bfaa1c8dfa7d1f84772a6cee7381568d5757718e
wanghao8080 <117663444@qq.com>
2018-09-06 09:27:51 +0800

    Future：异步程序

--- lib/demo/datetime_demo.dart
+++ lib/demo/datetime_demo.dart
@@ -11,7 +11,7 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
   DateTime selectedDate = DateTime.now();
   TimeOfDay selectedTime = TimeOfDay(hour: 9, minute: 30);
   
-  _selectDate() async {
+  Future<void> _selectDate() async {
     final DateTime date = await showDatePicker(
       context: context,
       initialDate: selectedDate,
@@ -26,7 +26,7 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
     });
   }
 
-  _selectTime() async {
+  Future<void> _selectTime() async {
     final TimeOfDay time = await showTimePicker(
       context: context,
       initialTime: selectedTime,
