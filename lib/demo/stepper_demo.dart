606fe76e289664efda622a9516745e33518218b2
wanghao8080 <117663444@qq.com>
2018-09-11 21:57:30 +0800

    Stepper：步骤

--- lib/demo/stepper_demo.dart
+++ lib/demo/stepper_demo.dart
@@ -18,7 +18,33 @@ class _StepperDemoState extends State<StepperDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
+            Theme(
+              data: Theme.of(context).copyWith(
+                primaryColor: Colors.black,
+              ),
+              child: Stepper(
+                steps: [
+                  Step(
+                    title: Text('Login'),
+                    subtitle: Text('Login first'),
+                    content: Text('Magna exercitation duis non sint eu nostrud.'),
+                    isActive: true,
+                  ),
+                  Step(
+                    title: Text('Choose Plan'),
+                    subtitle: Text('Choose you plan.'),
+                    content: Text('Magna exercitation duis non sint eu nostrud.'),
+                    isActive: true,
+                  ),
+                  Step(
+                    title: Text('Confirm payment'),
+                    subtitle: Text('Confirm your payment method.'),
+                    content: Text('Magna exercitation duis non sint eu nostrud.'),
+                    isActive: true,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       )
