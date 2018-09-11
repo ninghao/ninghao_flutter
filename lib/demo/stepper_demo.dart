6efb07281e4bcfa50f48a74e6ebf9d083e39d38e
wanghao8080 <117663444@qq.com>
2018-09-11 22:19:17 +0800

    Stepper：步骤的行为

--- lib/demo/stepper_demo.dart
+++ lib/demo/stepper_demo.dart
@@ -6,6 +6,8 @@ class StepperDemo extends StatefulWidget {
 }
 
 class _StepperDemoState extends State<StepperDemo> {
+  int _currentStep = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,24 +25,40 @@ class _StepperDemoState extends State<StepperDemo> {
                 primaryColor: Colors.black,
               ),
               child: Stepper(
+                currentStep: _currentStep,
+                onStepTapped: (int value) {
+                  setState(() {
+                    _currentStep = value;
+                  });
+                },
+                onStepContinue: () {
+                  setState(() {
+                    _currentStep < 2 ? _currentStep += 1 : _currentStep = 0;
+                  });
+                },
+                onStepCancel: () {
+                  setState(() {
+                    _currentStep > 0 ? _currentStep -= 1 : _currentStep = 0;
+                  });
+                },
                 steps: [
                   Step(
                     title: Text('Login'),
                     subtitle: Text('Login first'),
                     content: Text('Magna exercitation duis non sint eu nostrud.'),
-                    isActive: true,
+                    isActive: _currentStep == 0,
                   ),
                   Step(
                     title: Text('Choose Plan'),
                     subtitle: Text('Choose you plan.'),
                     content: Text('Magna exercitation duis non sint eu nostrud.'),
-                    isActive: true,
+                    isActive: _currentStep == 1,
                   ),
                   Step(
                     title: Text('Confirm payment'),
                     subtitle: Text('Confirm your payment method.'),
                     content: Text('Magna exercitation duis non sint eu nostrud.'),
-                    isActive: true,
+                    isActive: _currentStep == 2,
                   ),
                 ],
               ),
