802219f2bd4d4d02ec65958e00efe6fa2974891d
wanghao8080 <117663444@qq.com>
2018-09-11 12:32:57 +0800

    创建 PaginatedDataTableDemo

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -15,6 +15,7 @@ import './snack_bar_demo.dart';
 import './expansion_panel_demo.dart';
 import './chip_demo.dart';
 import './data_table_demo.dart';
+import './paginated_data_table_demo.dart';
 
 class MaterialComponents extends StatelessWidget {
   @override
@@ -26,6 +27,7 @@ class MaterialComponents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'PaginatedDataTable', page: PaginatedDataTableDemo()),
           ListItem(title: 'DataTable', page: DataTableDemo()),
           ListItem(title: 'Chip', page: ChipDemo()),
           ListItem(title: 'ExpansionPanel', page: ExpansionPanelDemo()),
---
+++ lib/demo/paginated_data_table_demo.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class PaginatedDataTableDemo extends StatefulWidget {
+  @override
+  _PaginatedDataTableDemoState createState() => _PaginatedDataTableDemoState();
+}
+
+class _PaginatedDataTableDemoState extends State<PaginatedDataTableDemo> {
+  int _sortColumnIndex;
+  bool _sortAscending = true;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PaginatedDataTableDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            DataTable(
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending,
+              // onSelectAll: (bool value) {},
+              columns: [
+                DataColumn(
+                  label: Text('Title'),
+                  onSort: (int index, bool ascending) {
+                    setState(() {
+                      _sortColumnIndex = index;
+                      _sortAscending = ascending;
+
+                      posts.sort((a, b) {
+                        if (!ascending) {
+                          final c = a;
+                          a = b;
+                          b = c;
+                        }
+
+                        return a.title.length.compareTo(b.title.length);
+                      });
+                    });
+                  },
+                ),
+                DataColumn(
+                  label: Text('Author'),
+                ),
+                DataColumn(
+                  label: Text('Image'),
+                ),
+              ],
+              rows: posts.map((post) {
+                return DataRow(
+                  selected: post.selected,
+                  onSelectChanged: (bool value) {
+                    setState(() {
+                      if (post.selected != value) {
+                        post.selected = value;
+                      }
+                    });
+                  },
+                  cells: [
+                    DataCell(Text(post.title)),
+                    DataCell(Text(post.author)),
+                    DataCell(Image.network(post.imageUrl)),
+                  ]
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
