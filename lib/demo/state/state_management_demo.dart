e5b365ace28c95871a0b644ade006d94e6366077
wanghao8080 <117663444@qq.com>
2018-09-25 13:38:45 +0800

    StatelessWidget：无变化状态的小部件

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 class StateManagementDemo extends StatelessWidget {
+  int count = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('StateManagementDemo'),
         elevation: 0.0,
+      ),
+      body: Center(
+        child: Chip(
+          label: Text('$count'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          count += 1;
+          print(count);
+        },
       ),
     );
   }
