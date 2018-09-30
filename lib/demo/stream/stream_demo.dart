9561bdbb5e1e642c1cecea09556d45b0a83b85bd
wanghao8080 <117663444@qq.com>
2018-09-30 15:01:33 +0800

    StreamController：创建可以多次订阅的 Broadcast Streams

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -37,12 +37,14 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
 
     print('Create a stream.');
     // Stream<String> _streamDemo = Stream.fromFuture(fetchData());
-    _streamDemo = StreamController<String>();
+    _streamDemo = StreamController.broadcast();
     _sinkDemo = _streamDemo.sink;
 
     print('Start listening on a stream.');
     _streamDemoSubscription =
         _streamDemo.stream.listen(onData, onError: onError, onDone: onDone);
+    
+    _streamDemo.stream.listen(onDataTwo, onError: onError, onDone: onDone);
 
     print('Initialize completed.');
   }
@@ -57,6 +59,10 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
 
   void onData(String data) {
     print('$data');
+  }
+
+  void onDataTwo(String data) {
+    print('onDataTwo: $data');
   }
 
   void _pauseStream() {
