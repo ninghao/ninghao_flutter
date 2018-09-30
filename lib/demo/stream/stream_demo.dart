331b4e4480c459ce0391c3307ebcd6502c22fad3
wanghao8080 <117663444@qq.com>
2018-09-30 14:34:45 +0800

    创建与监听 Stream

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StreamDemo extends StatelessWidget {
@@ -19,6 +21,28 @@ class StreamDemoHome extends StatefulWidget {
 }
 
 class _StreamDemoHomeState extends State<StreamDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+
+    print('Create a stream.');
+    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+
+    print('Start listening on a stream.');
+    _streamDemo.listen(onData);
+
+    print('Initialize completed.');
+  }
+
+  void onData(String data) {
+    print('$data');
+  }
+
+  Future<String> fetchData() async {
+    await Future.delayed(Duration(seconds: 3));
+    return 'hello ~';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
