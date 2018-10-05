c8ca90f314c5438e170c3dd6e6987525c74de53f
wanghao8080 <117663444@qq.com>
2018-10-05 12:35:11 +0800

    使用 Subjects 控制 Observables

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -25,14 +25,23 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
   void initState() {
     super.initState();
 
-    Observable<String> _observable = 
-        // Observable(Stream.fromIterable(['hello', '您好']));
-        // Observable.fromFuture(Future.value('hello ~'));
-        // Observable.fromIterable(['hello', '您好']);
-        // Observable.just('hello ~');
-        Observable.periodic(Duration(seconds: 3), (x) => x.toString());
+    // Observable<String> _observable = 
+    //     // Observable(Stream.fromIterable(['hello', '您好']));
+    //     // Observable.fromFuture(Future.value('hello ~'));
+    //     // Observable.fromIterable(['hello', '您好']);
+    //     // Observable.just('hello ~');
+    //     Observable.periodic(Duration(seconds: 3), (x) => x.toString());
 
-    _observable.listen(print);
+    // _observable.listen(print);
+
+    PublishSubject<String> _subject = PublishSubject<String>();
+
+    _subject.listen((data) => print('listen 1: $data'));
+    _subject.add('hello');
+    _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
+    _subject.add('hola');
+
+    _subject.close();
   }
 
   @override
