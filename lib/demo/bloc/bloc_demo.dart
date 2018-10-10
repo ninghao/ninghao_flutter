76b8872892938a45249350da9e096ec17e41cf5c
wanghao8080 <117663444@qq.com>
2018-10-10 14:04:42 +0800

    使用 InheritedWidget 传递 BLoC

--- lib/demo/bloc/bloc_demo.dart
+++ lib/demo/bloc/bloc_demo.dart
@@ -4,13 +4,16 @@ import 'package:ninghao_flutter/demo/bloc/counter_bloc_demo.dart';
 class BlocDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BlocDemo'),
-        elevation: 0.0,
+    return CounterProvider(
+      bloc: CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BlocDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterHome(),
+        floatingActionButton: CounterActionButton(),
       ),
-      body: CounterHome(),
-      floatingActionButton: CounterActionButton(),
     );
   }
 }
--- lib/demo/bloc/counter_bloc_demo.dart
+++ lib/demo/bloc/counter_bloc_demo.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+    
     return Center(
       child: ActionChip(
         label: Text('0'),
-        onPressed: () {},
+        onPressed: () {
+          _counterBloc.log();
+        },
       ),
     );
   }
@@ -15,9 +19,37 @@ class CounterHome extends StatelessWidget {
 class CounterActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CounterBloc _counterBloc = CounterProvider.of(context).bloc;
+    
     return FloatingActionButton(
       child: Icon(Icons.add),
-      onPressed: () {},
+      onPressed: () {
+        _counterBloc.log();
+      },
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final Widget child;
+  final CounterBloc bloc; 
+
+  CounterProvider({
+    this.child,
+    this.bloc,
+  }) : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  bool updateShouldNotify(CounterProvider oldWidget) {
+    return true;
+  }
+}
+
+class CounterBloc {
+  void log() {
+    print('BLoC');
   }
 }
