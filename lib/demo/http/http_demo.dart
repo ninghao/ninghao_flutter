38897cbd2ab48b7a6c1277530da9422e7413bf7a
wanghao8080 <117663444@qq.com>
2018-10-11 09:20:37 +0800

    创建 HttpDemo

---
+++ lib/demo/http/http_demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HttpDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ninghao_flutter/demo/bloc/bloc_demo.dart';
+import 'package:ninghao_flutter/demo/http/http_demo.dart';
 import 'package:ninghao_flutter/demo/rxdart/rxdart_demo.dart';
 import 'package:ninghao_flutter/demo/state/state_management_demo.dart';
 import 'package:ninghao_flutter/demo/stream/stream_demo.dart';
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: NavigatorDemo(),
-      initialRoute: '/bloc',
+      initialRoute: '/http',
       routes: {
         '/': (context) => Home(),
         '/about': (context) => Page(title: 'About'),
@@ -32,6 +33,7 @@ class App extends StatelessWidget {
         '/stream': (context) => StreamDemo(),
         '/rxdart': (context) => RxDartDemo(),
         '/bloc': (context) => BlocDemo(),
+        '/http': (context) => HttpDemo(),
       },
       theme: ThemeData(
         primarySwatch: Colors.yellow,
