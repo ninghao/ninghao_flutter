16a257d371318ba50ecbcbf79d50d15754a28963
wanghao8080 <117663444@qq.com>
2018-10-11 10:02:14 +0800

    使用 http 客户端请求数据

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HttpDemo extends StatelessWidget {
   @override
@@ -8,6 +10,33 @@ class HttpDemo extends StatelessWidget {
         title: Text('HttpDemo'),
         elevation: 0.0,
       ),
+      body: HttpDemoHome(),
     );
+  }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+    fetchPost();
+  }
+
+  fetchPost() async {
+    final response = 
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    
+    print('statusCode: ${response.statusCode}');
+    print('body: ${response.body}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
