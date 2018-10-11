133ef3e7e13e5316035eda161558a6532e65e04a
wanghao8080 <117663444@qq.com>
2018-10-11 15:09:13 +0800

    使用请求数据：把响应的数据转换成自定义 Dart 对象

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -26,36 +26,48 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-    // fetchPost();
+    fetchPosts()
+      .then((value) => print(value));
 
-    final post = {
-      'title': 'hello',
-      'description': 'nice to meet you.',
-    };
+    // final post = {
+    //   'title': 'hello',
+    //   'description': 'nice to meet you.',
+    // };
 
-    print(post['title']);
-    print(post['description']);
+    // print(post['title']);
+    // print(post['description']);
 
-    final postJson = json.encode(post);
-    print(postJson);
+    // final postJson = json.encode(post);
+    // print(postJson);
 
-    final postJsonConverted = json.decode(postJson);
-    print(postJsonConverted['title']);
-    print(postJsonConverted['description']);
-    print(postJsonConverted is Map);
+    // final postJsonConverted = json.decode(postJson);
+    // print(postJsonConverted['title']);
+    // print(postJsonConverted['description']);
+    // print(postJsonConverted is Map);
 
-    final postModel = Post.fromJson(postJsonConverted);
-    print('title: ${postModel.title}, description: ${postModel.description}');
+    // final postModel = Post.fromJson(postJsonConverted);
+    // print('title: ${postModel.title}, description: ${postModel.description}');
 
-    print('${json.encode(postModel)}');
+    // print('${json.encode(postModel)}');
   }
 
-  fetchPost() async {
+  Future<List<Post>> fetchPosts() async {
     final response = 
         await http.get('https://resources.ninghao.net/demo/posts.json');
     
-    print('statusCode: ${response.statusCode}');
-    print('body: ${response.body}');
+    // print('statusCode: ${response.statusCode}');
+    // print('body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+        .map<Post>((item) => Post.fromJson(item))
+        .toList();
+
+      return posts;
+    } else {
+      throw Exception('Failed to fetch posts.');
+    }
   }
 
   @override
@@ -65,17 +77,26 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
 }
 
 class Post {
+  final int id;
   final String title;
   final String description;
+  final String author;
+  final String imageUrl;
 
   Post(
+    this.id,
     this.title,
-    this.description
+    this.description,
+    this.author,
+    this.imageUrl,
   );
 
   Post.fromJson(Map json)
-    : title = json['title'],
-      description = json['description'];
+    : id = json['id'],
+      title = json['title'],
+      description = json['description'],
+      author = json['author'],
+      imageUrl = json['imageUrl'];
 
   Map toJson() => {
     'title': title,
