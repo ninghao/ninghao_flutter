5f2319e11c13b463d0599f72e2ffd423ec491b84
wanghao8080 <117663444@qq.com>
2018-10-15 18:10:20 +0800

    AnimationController：动画控制器

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -4,12 +4,11 @@ class AnimationDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AnimationDemo'),
-        elevation: 0.0,
-      ),
-      body: AnimationDemoHome(),
-    );
+        appBar: AppBar(
+          title: Text('AnimationDemo'),
+          elevation: 0.0,
+        ),
+        body: AnimationDemoHome());
   }
 }
 
@@ -18,7 +17,33 @@ class AnimationDemoHome extends StatefulWidget {
   _AnimationDemoHomeState createState() => _AnimationDemoHomeState();
 }
 
-class _AnimationDemoHomeState extends State<AnimationDemoHome> {
+class _AnimationDemoHomeState extends State<AnimationDemoHome>
+    with TickerProviderStateMixin {
+  AnimationController animationDemoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationDemoController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    animationDemoController.addListener(() {
+      print('${animationDemoController.value}');
+    });
+
+    animationDemoController.forward();
+  }
+
+  @override
+  void dispose() { 
+    super.dispose();
+
+    animationDemoController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
