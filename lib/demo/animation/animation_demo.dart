4bfab7efd8c14eba43ce1c0aee2415a9e838909a
wanghao8080 <117663444@qq.com>
2018-10-16 07:53:48 +0800

    Tween：设置动画范围值（数字、颜色）

--- lib/demo/animation/animation_demo.dart
+++ lib/demo/animation/animation_demo.dart
@@ -20,18 +20,24 @@ class AnimationDemoHome extends StatefulWidget {
 class _AnimationDemoHomeState extends State<AnimationDemoHome>
     with TickerProviderStateMixin {
   AnimationController animationDemoController;
+  Animation animation;
+  Animation animationColor;
 
   @override
   void initState() {
     super.initState();
 
     animationDemoController = AnimationController(
-      value: 32.0,
-      lowerBound: 32.0,
-      upperBound: 100.0,
-      duration: Duration(milliseconds: 3000),
+      // value: 32.0,
+      // lowerBound: 32.0,
+      // upperBound: 100.0,
+      duration: Duration(milliseconds: 1000),
       vsync: this,
     );
+
+    animation = Tween(begin: 32.0, end: 100.0).animate(animationDemoController);
+    animationColor = 
+        ColorTween(begin: Colors.red, end: Colors.red[900]).animate(animationDemoController);
 
     animationDemoController.addListener(() {
       // print('${animationDemoController.value}');
@@ -57,7 +63,8 @@ class _AnimationDemoHomeState extends State<AnimationDemoHome>
     return Center(
       child: IconButton(
         icon: Icon(Icons.favorite),
-        iconSize: animationDemoController.value,
+        iconSize: animation.value,
+        color: animationColor.value,
         onPressed: () {
           switch (animationDemoController.status) {
             case AnimationStatus.completed:
