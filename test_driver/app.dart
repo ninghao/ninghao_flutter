31893246efd49991fe332ded1c64fd099a8f2786
wanghao8080 <117663444@qq.com>
2018-10-22 15:27:14 +0800

    Integration testing：集成测试

---
+++ test_driver/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:ninghao_flutter/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+
+  app.main();
+}
---
+++ test_driver/app_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('App', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+  });
+}
